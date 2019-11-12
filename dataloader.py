548b0e6427a3b9699095cb9e90f52a486147f7b0
Oishi <oishib@users.noreply.github.com>
2019-11-12 11:32:56 -0800

    Added Embeddings

--- dataloader.py
+++ dataloader.py
@@ -427,5 +427,42 @@ def show_samples(dataset, num_samples):
             plt.show()
             break
 
+
+#detach, move to cpu, call numpy, numpy.save(path, array)
+#simultaneously save outputs
+#embedding dataset which reads the numpy arrays at initialization
+#implement len
+#implement get_item
+
+class EmbeddingDataset(Dataset):
+    def __init__(self, labels_path, ns_labels_path, s_labels_path, embeddings_path):
+        self.labels = np.load(labels_path)
+        self.embeddings = np.load(embeddings_path)
+        self.ns_labels = np.load(ns_labels_path)
+        self.s_labels = np.load(s_labels_path)
+    def __len__(self):
+        return self.labels.size
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        embed = self.embeddings[idx]
+        #embed = 0
+        return {'pm':label,'embed':embed,'ns_pred':self.ns_labels[idx],'s_pred':self.s_labels[idx]}
+
+def load_embeddings(labels_path, ns_labels_path, s_labels_path, embeddings_path,batch_size):
+    dataset = EmbeddingDataset(labels_path, ns_labels_path, s_labels_path, embeddings_path)
+    full_ds_size = len(dataset) 
+    indices = list(range(full_ds_size))
+    split1, split2 = 64*240,64*300
+    train_indices, val_indices, test_indices = indices[:split1], indices[split1:split2], indices[split2:]
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(val_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=train_sampler, num_workers=4)
+    val_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=val_sampler, num_workers=2)
+    test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=test_sampler, num_workers=0)
+    
+    dataloaders = { 'train': train_dataloader, 'val': val_dataloader, 'test': test_dataloader}
+    
+    return dataloaders
             
 
