fixed normalization for cnn/cnn_combined and added additional util functions

cnn.py
@@ -22,7 +22,7 @@ class Small_CNN(nn.Module):
     def __init__(self, device = "cpu"):
         super(Small_CNN, self).__init__()
 
-        in_channels = 7 #8# 13
+        in_channels = 8 
         out_channels1 = 64
         out_channels2 = 128
         out_channels3 = 256
@@ -43,8 +43,8 @@ class Small_CNN(nn.Module):
         self.bn4 = nn.BatchNorm2d(out_channels4)
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.drop = nn.Dropout(p=0.2)
-        self.fc1 = nn.Linear(256 * 8 * 8, 4000) #128 * 24 * 24
+        self.drop = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(256 * 8 * 8, 4000)
         self.fc2 = nn.Linear(4000, 100)
         self.fc3 = nn.Linear(100, 1) 
 
@@ -63,7 +63,7 @@ class Small_CNN(nn.Module):
         x = F.relu(self.bn4(x))
         x = self.pool4(x)        
         x = self.drop(x)
-        x = x.reshape(x.size(0), 256 * 8 * 8)  #128 * 24 * 24
+        x = x.reshape(x.size(0), 256 * 8 * 8) 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -93,8 +93,6 @@ def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=No
     summaries  = []
     num_batches = len(dataloader)
     train_dataset_size = num_batches * batch_size
-    batch_no = 0
-    num_batches = min(num_batches, 2000)
     
     # Set model to train mode
     model.train()
@@ -120,13 +118,14 @@ def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=No
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if scheduler != None:
-                scheduler.step()
+            #if scheduler != None:
+            #    scheduler.step()
                 
             # Move to cpu and convert to numpy
             outputs = outputs.data.cpu().numpy()
             labels = labels.data.cpu().numpy()
             indices = indices.data.cpu().numpy()
+            sites, dates = sites.data.cpu().numpy(), dates.data.cpu().numpy()
             
             # Compute batch metrics
             r2 = r2_score(labels, outputs) 
@@ -139,16 +138,12 @@ def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=No
             t.set_postfix(loss=loss_str, r2=r2_str) 
             t.update()
             
-            if epoch % 25 == 0:
+            if epoch % 20 == 0:
             # Save predictions to compute r2 over full dataset
                 curr_batch_size = outputs.shape[0]  
                 utils.save_predictions(indices, outputs, labels, sites, dates, curr_batch_size, 
                                        "predictions/cnn_train_preds_epoch_" + str(epoch) + ".csv") 
-            
-            batch_no += 1
-            if batch_no == 2000:
-                break
-            
+         
             del inputs, labels, outputs
             torch.cuda.empty_cache()
             
@@ -268,14 +263,10 @@ def train_and_evaluate(model, optimizer, loss_fn, train_dataloader, val_dataload
             best_json_path = os.path.join(model_dir, "metrics_val_best_weights_regression_unbal.json")
             utils.save_dict_to_json(val_mean_metrics, best_json_path)
         
-        if epoch % 20 ==0:
+        if epoch % 10 == 0:
             print("Train losses: {} \n Validation losses: {}".format(all_train_losses, all_val_losses))
             print("Train mean R2s: {} \n Validation mean R2s: {}".format(all_train_r2, all_val_r2))
-            #utils.plot_losses(all_train_losses, all_val_losses, epoch, num_train,
-            #                  save_as="plots/loss_cnn_" + str(num_train) +"epoch"+ str(epoch) +".png")
-            #utils.plot_r2(all_train_r2, all_val_r2, epoche, num_train, 
-            #                 save_as="plots/r2_cnn_" + str(num_train) +"epoch"+ str(epoch) +".png")
-        
+            
             
     print("Train losses: {} ".format(all_train_losses))
     print("Val losses: {} ".format(all_val_losses))
@@ -300,7 +291,7 @@ def predict(model, loss_fn, dataloader, batch_size, num_epochs,
 
 
     # Evaluate on validation or test set
-    epoch = "--"
+    epoch = "all_val_threshold"
     mean_metrics = evaluate(model, loss_fn, dataloader, batch_size, epoch)
     r2 = mean_metrics['average r2']
     print("Mean R2 for {} dataset: {}".format(dataset, r2))
@@ -308,25 +299,26 @@ def predict(model, loss_fn, dataloader, batch_size, num_epochs,
     
 if __name__ == "__main__":
     
-    cleaned_csv = "data_csv_files/master_csv_with_averages.csv"
     npy_dir = '/home/sarahciresi/gcloud/cs325b-airquality/cs325b/images/s2/'
-    sent_dir = "/home/sarahciresi/gcloud/cs325b-airquality/cs325b/data/sentinel/2016/"
     checkpt_dir = "/home/sarahciresi/gcloud/cs325b-airquality/checkpt_small_cnn/"
     train_csv = "train_sites_master_csv_2016.csv"
-    val_csv = "mini_val_sites_shuffled5000_2016.csv" #"val_sites_master_csv_2016.csv"
+    val_csv = "val_sites_master_csv_2016.csv"
+    mini_val_csv = "mini_val_sites_shuffled5000_2016.csv" 
+    mini_train_csv = "mini_train_sites_shuffled15000_2016.csv"
     
     lr = 0.00001
-    reg = 1e-5
-    batch_size = 64
+    reg = 5e-2
+    batch_size = 90
     num_epochs = 100 
-    num_train = 154665
+    num_train = 107376 #154665
 
-    print("Training model for {} epochs with batch size: {}, lr: {}, reg: {} using {} training examples.".format(num_epochs, batch_size, lr, reg, num_train))
+    print("Training for {} epochs with batch size: {}, lr: {}, reg: {} using {} training examples.".format(num_epochs, batch_size, lr, reg, num_train))
     
-    #dataloaders = load_data(cleaned_csv, npy_dir, sent_dir, batch_size=batch_size, sample_balanced=True)
-    dataloaders = load_data_new(train_csv, batch_size = batch_size, num_workers=8,
-                                train_images=npy_dir, val_images=npy_dir,
-                                val_nonimage_csv=val_csv)  
+    dataloaders = load_data_new(train_csv, batch_size = batch_size, 
+                                sample_balanced=False, num_workers=8,
+                                train_images=npy_dir, val_images=npy_dir, 
+                                val_nonimage_csv=val_csv)    
+    
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = Small_CNN(device) 
@@ -334,15 +326,26 @@ if __name__ == "__main__":
     model._set_seeds(0)
     model.apply(model.init_weights)
     optimizer = optim.Adam(model.parameters(), lr = lr, weight_decay=reg)
-
-
-    start_time = time.time()
     
     train_and_evaluate(model, optimizer, nn.MSELoss(), dataloaders['train'], dataloaders['val'], 
-                       batch_size=batch_size, num_epochs=num_epochs, num_train=num_train, model_dir = checkpt_dir)     
+                      batch_size=batch_size, num_epochs=num_epochs, num_train=num_train, model_dir = checkpt_dir)     
+   
+    '''
+    predict(model, nn.MSELoss(), dataloaders['train'], batch_size=batch_size, num_epochs=num_epochs, 
+            dataset='train', model_dir=checkpt_dir, saved_weights_file="weights")
                        
     
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print("done")
-
-
+    
+    preds="predictions/cnn_val_preds_epoch_all_val_threshold.csv"
+    monthly_preds = "predicted_and_true_avgs_csv.csv"
+    r2, pearson = utils.compute_r2(preds) 
+    print(r2)
+    print(pearson)
+    
+    utils.compute_pm_month_average_post(preds, cleaned_csv, averages_csv="averages.csv")
+    utils.plot_predictions(preds)
+    r2, pearson = utils.compute_r2_monthly("predicted_and_true_avgs_csv.csv")
+    '''
+    
+    
+  

cnn_combined.py
@@ -3,6 +3,7 @@ import csv
 import time
 import pandas
 import random
+import datetime
 import numpy as np
 from tqdm import tqdm
 
@@ -17,23 +18,26 @@ from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 from dataloader import load_data, load_data_new
 from pandarallel import pandarallel
+#from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 import utils
+
 
 class CNN_combined(nn.Module):
     def __init__(self, device = "cpu"):
         super(CNN_combined, self).__init__()
 
-        in_channels = 13 #4
+        in_channels = 8 #7 #13
         out_channels1 = 64 #64
         out_channels2 = 128 #128
         out_channels3 = 256 #256
         out_channels4 = 256
-        num_ff_features = 14 #16
+        num_ff_features = 16 #14
         
         self.device = device
         
         # Conv portion
-        self.conv1 = nn.Conv2d(in_channels, out_channels1, kernel_size=5, stride=1, padding=1) # 11
+        self.conv1 = nn.Conv2d(in_channels, out_channels1, kernel_size=5, stride=1, padding=2) # 11
         self.bn1 = nn.BatchNorm2d(out_channels1)
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=3)
         self.conv2 = nn.Conv2d(out_channels1, out_channels2, kernel_size=3, stride=1, padding=1) #7
@@ -45,13 +49,13 @@ class CNN_combined(nn.Module):
         self.conv4 = nn.Conv2d(out_channels3, out_channels4, kernel_size=3, stride=1, padding=1) #5
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.drop = nn.Dropout(p=0.2)
+        self.drop = nn.Dropout(p=0.5) # p=0.5
         
         # Feed forward portion
         self.fffc1 = nn.Linear(num_ff_features, 500)
         self.fffc2 = nn.Linear(500,500)
         self.fffc3 = nn.Linear(500,100)
-        self.dropfffc = nn.Dropout(p=0.2)
+        self.dropfffc = nn.Dropout(p=0.5) 
         
         # Recombined portion
         ##self.fc1 = nn.Linear(128 * 24 * 24 + 100, 200)
@@ -76,10 +80,9 @@ class CNN_combined(nn.Module):
         x1 = self.conv3(x1)
         x1 = F.relu(self.bn3(x1))
         x1 = self.pool3(x1)
-        
         x1 = F.relu(self.conv4(x1))
         x1 = self.pool4(x1)
-        x1 = x1.reshape(x1.size(0), 256*8*8) # 128 * 24 * 24)
+        x1 = x1.reshape(x1.size(0), 256*8*8) 
         x1 = self.drop(x1)
         
         # FF 
@@ -106,7 +109,7 @@ class CNN_combined(nn.Module):
         random.seed(seed)
         np.random.seed(seed)
 
-def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=None):
+def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, t_global_step):
     '''
     Trains the model for 1 epoch on all batches in the dataloader.
     '''
@@ -126,27 +129,30 @@ def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=No
         for i, sample in enumerate(dataloader):
             
             indices, inputs, features, labels = sample['index'], sample['image'], sample['non_image'], sample['label']
+            sites, dates = sample['site'], sample['month']
             
             # Move to GPU if available       
             inputs = inputs.to(model.device, dtype=torch.float)
             labels = labels.to(model.device, dtype=torch.float)
             features = features.to(model.device, dtype=torch.float)
-
+                       
             # Forward pass and calculate loss
             outputs = model(inputs, features) 
+            #loss = loss_fn(outputs, labels, t_global_step, dataset='train')
             loss = loss_fn(outputs, labels)
-
+            
             # Compute gradients and perform parameter updates
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if scheduler != None:
-                scheduler.step()
+            #if scheduler != None:
+            #    scheduler.step()
        
             # Move to cpu and convert to numpy
             outputs = outputs.data.cpu().numpy()
             labels = labels.data.cpu().numpy()
             indices = indices.data.cpu().numpy()
+            sites, dates = sites.data.cpu().numpy(), dates.data.cpu().numpy()
 
             # Save predictions to compute r2 over full dataset
             curr_batch_size = outputs.shape[0] # if last batch, may be less than full batch size
@@ -164,6 +170,16 @@ def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=No
             t.set_postfix(loss=loss_str, r2=r2_str) 
             t.update()
             
+            if epoch % 10 == 0:
+            # Save predictions to compute r2 over full dataset
+                curr_batch_size = outputs.shape[0]  
+                utils.save_predictions(indices, outputs, labels, sites, dates, curr_batch_size, 
+                                       "predictions/newest_combined_train_epoch_" + str(epoch) + ".csv") 
+            
+            writer.add_scalar('train/loss', loss, t_global_step)
+            writer.add_scalar('train/r2', r2, t_global_step)
+            t_global_step += 1
+                
             del inputs, features, labels, outputs
             torch.cuda.empty_cache()
   
@@ -172,10 +188,10 @@ def train(model, optimizer, loss_fn, dataloader, batch_size, epoch, scheduler=No
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in mean_metrics.items())
     print("Train metrics: {}".format(metrics_string))
 
-    return mean_metrics
+    return mean_metrics, t_global_step
 
 
-def evaluate(model, loss_fn, dataloader, batch_size, epoch):
+def evaluate(model, loss_fn, dataloader, batch_size, epoch, v_global_step):
     '''
     Evaluates the model for 1 epoch on all batches in the dataloader.
     '''
@@ -194,25 +210,28 @@ def evaluate(model, loss_fn, dataloader, batch_size, epoch):
             for i, sample in enumerate(dataloader):
                 
                 indices, inputs, features, labels = sample['index'], sample['image'], sample['non_image'], sample['label']
+                sites, dates = sample['site'], sample['month']
 
                 # Move to GPU if available       
                 inputs = inputs.to(model.device, dtype=torch.float)
                 labels = labels.to(model.device, dtype=torch.float)
                 features = features.to(model.device, dtype=torch.float)
-
+                                
                 # Forward pass and calculate loss
                 outputs = model(inputs, features) 
+                #loss = loss_fn(outputs, labels, v_global_step, dataset='val')
                 loss = loss_fn(outputs, labels)
-
+                
                 # Move to cpu and convert to numpy
                 outputs = outputs.data.cpu().numpy()
                 labels = labels.data.cpu().numpy()
                 indices = indices.data.cpu().numpy()
+                sites, dates = sites.data.cpu().numpy(), dates.data.cpu().numpy()
                 
                 # Save predictions to compute r2 over full dataset
                 curr_batch_size = outputs.shape[0]  
-                utils.save_predictions(indices, outputs, labels, curr_batch_size, 
-                                       "predictions/monthly_val_preds_shuffled_epoch_" + str(epoch) + ".csv") 
+                utils.save_predictions(indices, outputs, labels, sites, dates, curr_batch_size, 
+                                       "predictions/newest_combined_val_epoch_" + str(epoch) + ".csv") 
           
                 # Compute batch metrics
                 r2 = r2_score(labels, outputs) #.cpu().detach().numpy())
@@ -225,6 +244,10 @@ def evaluate(model, loss_fn, dataloader, batch_size, epoch):
                 t.set_postfix(loss=loss_str, r2=r2_str) 
                 t.update()
                 
+                writer.add_scalar('val/loss', loss, v_global_step)
+                writer.add_scalar('val/r2', r2, v_global_step)
+                v_global_step +=1
+
                 del inputs, features, labels, outputs
                 torch.cuda.empty_cache()
    
@@ -232,7 +255,7 @@ def evaluate(model, loss_fn, dataloader, batch_size, epoch):
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in mean_metrics.items())
     print("Evaluation metrics: {}".format(metrics_string))
     
-    return mean_metrics
+    return mean_metrics, v_global_step
 
 
 def train_and_evaluate(model, optimizer, loss_fn, train_dataloader, val_dataloader, 
@@ -240,8 +263,9 @@ def train_and_evaluate(model, optimizer, loss_fn, train_dataloader, val_dataload
     '''
     Trains the model and evaluates at every epoch
     '''
-
-    best_val_r2 = 0.0
+    t_global = 0
+    v_global = 0
+    best_val_r2 = -1.0
     all_train_losses, all_val_losses, all_train_r2, all_val_r2 = [], [], [], []
     
     # If a saved weights file for the model is specified, reload the weights
@@ -253,12 +277,14 @@ def train_and_evaluate(model, optimizer, loss_fn, train_dataloader, val_dataload
     for epoch in range(num_epochs):
         
         print("Running Epoch {}/{}".format(epoch, num_epochs))
-              
+       
+        epoch_start_time = time.time()
+            
         # Train model for one epoch
-        train_mean_metrics = train(model, optimizer, loss_fn, train_dataloader, batch_size, epoch)
+        train_mean_metrics, t_global = train(model, optimizer, loss_fn, train_dataloader, batch_size, epoch, t_global)
 
         # Evaluate on validation set
-        val_mean_metrics = evaluate(model, loss_fn, val_dataloader, batch_size, epoch)
+        val_mean_metrics, v_global = evaluate(model, loss_fn, val_dataloader, batch_size, epoch, v_global)
         
         # Save losses and r2 from this epoch
         all_train_losses.append( train_mean_metrics['average MSE loss'] )
@@ -287,15 +313,18 @@ def train_and_evaluate(model, optimizer, loss_fn, train_dataloader, val_dataload
             print("Train losses: {} \n Validation losses: {}".format(all_train_losses, all_val_losses))
             print("Train mean R2s: {} \n Validation mean R2s: {}".format(all_train_r2, all_val_r2))
        
+        print("Epoch took --- %s seconds ---" % (time.time() - start_time))
+
+
     # Print average losses and R2 over train and validation sets
     print("Train losses: {} \n Validation losses: {}".format(all_train_losses, all_val_losses))
     print("Train mean R2s: {} \n Validation mean R2s: {}".format(all_train_r2, all_val_r2))
         
     # Plot losses and R2 over train and validation sets   
     utils.plot_losses(all_train_losses, all_val_losses, num_epochs, num_train, 
-                      save_as="plots/loss_"+str(num_train)+"ex_unb.png")
+                      save_as="plots/loss_"+str(num_train)+"ex.png")
     utils.plot_r2(all_train_r2, all_val_r2, num_epochs, num_train, 
-                  save_as="plots/r2_"+str(num_train)+"ex_unb.png")
+                  save_as="plots/r2_"+str(num_train)+"ex.png")
                     
     # Return train and eval metrics
     return train_mean_metrics, val_mean_metrics
@@ -318,55 +347,104 @@ def predict(model, loss_fn, dataloader, batch_size, num_epochs,
     r2 = mean_metrics['average r2']
     print("Mean R2 for {} dataset: {}".format(dataset, r2))
     
+    
+    
+def weighted_mse_loss(inputs, targets, global_cnt, dataset):
+    
+    zeros = torch.zeros_like(inputs)
+    above_15_targets = torch.where(targets>15, targets, zeros)  
+    middle_targets = torch.where(targets>= 5, targets, zeros) 
+    middle_targets =  torch.where(middle_targets<=15, targets, zeros)
+    below_5_targets = torch.where(targets<5, targets, zeros) 
+    
+    above_15_inputs = torch.where(targets>15, inputs, zeros)  
+    middle_inputs = torch.where(targets>= 5, inputs, zeros) 
+    middle_inputs =  torch.where(middle_targets<=15, inputs, zeros)
+    below_5_inputs = torch.where(targets<5, inputs, zeros) 
+    
+    loss_above = torch.sum(0.45 *(above_15_inputs - above_15_targets) ** 2)
+    loss_middle = torch.sum(0.1 *(middle_inputs - middle_targets) ** 2)
+    loss_below = torch.sum(0.45 *(below_5_inputs - below_5_targets) ** 2)
+    
+    writer.add_scalar(dataset+"/loss_low", loss_below, global_cnt )
+    writer.add_scalar(dataset+"/loss_mid", loss_middle, global_cnt)
+    writer.add_scalar(dataset+"/loss_high", loss_above, global_cnt)
+    
+   
+    #print("Loss above: {}, loss middle: {}, loss below: {}".format(loss_above,loss_middle,loss_below))
+    #print("\n")
+    
+    loss = loss_above + loss_middle + loss_below
+    '''
+    loss = torch.sum(0.45 *(above_15_inputs - above_15_targets) ** 2
+                     + 0.1 *(middle_inputs - middle_targets) ** 2
+                     + 0.45 *(below_5_inputs - below_5_targets) ** 2
+                    )
+     '''
+                    
+    return loss
+
 
 if __name__ == "__main__":
     
-    #save_to_csv= "data_csv_files/cleaned_data_all_temp_new_3.csv"
-    #utils.remove_sent_and_save_df_to_csv(cleaned_csv, save_to_csv)
-    
-    #cleaned_csv = "data_csv_files/cleaned_data_all_temp_new_3.csv"
     cleaned_csv = "data_csv_files/master_csv_with_averages.csv"
-    train_csv = "mini_train_sites_15000_2016.csv"
-    val_csv = "mini_val_sites_5000_2016.csv"
+    train_csv = "train_sites_master_csv_2016.csv"
+    val_csv = "val_sites_master_csv_2016.csv"
+    mini_val_csv = "mini_val_sites_shuffled5000_2016.csv"  
+    mini_train_csv = "mini_train_sites_shuffled15000_2016.csv"
     npy_dir = '/home/sarahciresi/gcloud/cs325b-airquality/cs325b/images/s2/'
-    sent_dir = "/home/sarahciresi/gcloud/cs325b-airquality/cs325b/data/sentinel/2016/"
-    chckpt_dir = "/home/sarahciresi/gcloud/cs325b-airquality/new_checkpoint/"
+    chckpt_dir = "/home/sarahciresi/gcloud/cs325b-airquality/new_checkpoint3/"   #checkpoint 2 has positive val r2s
     
-    lr = 0.00001 #0.00003 # 0.00001
-    reg = 1e-4
+    log_dir="logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    writer = SummaryWriter() #log_dir)
+
+    lr = 0.00009 #main 0.0001 ##try 0.0005, then 0.00001 
+    reg = 5e-2
     batch_size = 90
-    num_epochs = 100
-    num_train = 15000
+    num_epochs = 40
+    num_train = 92875+14501
    
     print("Training model for {} epochs with batch size = {}, lr = {}, reg = {} using {} training examples.".format(num_epochs, batch_size, lr, reg, num_train))
    
-    #dataloaders = load_data(cleaned_csv, npy_dir, sent_dir, batch_size=batch_size, sample_balanced=True, predict_monthly=True)
-    # old #dataloaders = load_data(cleaned_csv, npy_dir, sent_dir, batch_size=batch_size, predict_monthly=True)
-
-    dataloaders = load_data_new(train_csv, batch_size = batch_size, num_workers=8,
-                                predict_monthly=True, train_images=npy_dir,
-                                val_images=npy_dir, val_nonimage_csv=val_csv)    
+    dataloaders = load_data_new(train_csv, batch_size = batch_size, 
+                                sample_balanced=False, num_workers=8,
+                                train_images=npy_dir, val_images=npy_dir, val_nonimage_csv=val_csv)    
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = CNN_combined(device)
     model.to(device)
+
     model._set_seeds(0)
     model.apply(model.init_weights)
     optimizer = optim.Adam(model.parameters(), lr = lr, weight_decay=reg)
     
     start_time = time.time()
-    '''
+    
     train_and_evaluate(model, optimizer, nn.MSELoss(), dataloaders['train'], dataloaders['val'], 
                        batch_size=batch_size, num_epochs=num_epochs, num_train=num_train, 
-                       model_dir = chckpt_dir) 
+                       model_dir = chckpt_dir, saved_weights_file="all_time_best_2")
     
-    predict(model, nn.MSELoss(), dataloaders['val'], batch_size, num_epochs, 
-            dataset='val', model_dir=chckpt_dir, saved_weights_file="all_time_best_monthly")
-    '''
-  
+    # predict(model, nn.MSELoss(), dataloaders['val'], batch_size, num_epochs, 
+    #        dataset='val', model_dir=chckpt_dir, saved_weights_file="last_6")
+   
+    #preds = "predictions/combined_val--.csv"
+    #r2, pearson = utils.compute_r2(preds) 
+    #print(r2)
+    #print(pearson)
+    
     print("done")
     print("--- %s seconds ---" % (time.time() - start_time))
+    '''
+    val_preds = "predictions/combined_val_old3.csv"
+    utils.plot_predictions_histogram(val_preds, 'val')
 
-    preds = "predictions/cnn_val_preds_epoch_0.csv"
-    utils.compute_pm_month_average_post(preds, cleaned_csv)
+    train_preds= "predictions/combined_train_preds_epoch_0.csv"
+    utils.plot_predictions_histogram(train_preds, 'train')
+    '''
+
+    #utils.compute_pm_month_average_post(preds, cleaned_csv)
+
     
+    
+
+    writer.close()

dataloader.py
@@ -22,18 +22,16 @@ class ToTensor(object):
         tensors = {"index": sample["index"], "month": sample["month"], "site": sample["site"],
                    "non_image" : torch.from_numpy(np.asarray(sample["non_image"])).to(dtype=torch.float),
                    "label" : torch.from_numpy(np.asarray(sample["label"])).to(dtype=torch.float)}
-        #tensors = {"label" : torch.from_numpy(np.asarray(sample["label"])).to(dtype=torch.float)}
         
         if "image" in sample:
             # Swap channel axis from H x W x C  to  C x H x W
             image = sample["image"].transpose((2, 0, 1))
             
             # Remove bands?  keep 0-3, 9-11
-            first_4_bands = image[:4] 
-            last_3_bands = image[9:12]
-            image = np.concatenate((first_4_bands, last_3_bands), axis=0)
+            #first_4_bands = image[:4] 
+            #last_3_bands = image[9:12] #[9:12]
+            #image = np.concatenate((first_4_bands, last_3_bands), axis=0)
             
-            #
             tensors["image"] = torch.from_numpy(np.asarray(image)).to(dtype=torch.float)
 
         if "non_image" in sample:
@@ -99,12 +97,15 @@ class CombinedDataset(Dataset):
         self.epa_df = pd.read_csv(master_csv_file)
         #self.epa_df = self.epa_df.sample(frac=1) # shuffle the df
         self.image_dir = image_dir
-        self.transform = transforms.Compose([ToTensor()])
+        self.transform = transforms.Compose([ToTensor(), Normalize()])
         self.classify = classify
         self.predict_monthly = predict_monthly
 
+        self.epa_df = self.epa_df[self.epa_df['PRCP']==0]   
+        
         if threshold != None:
             self.epa_df = self.epa_df[self.epa_df['Daily Mean PM2.5 Concentration'] < threshold]
+            print("Thresholding at {}".format(threshold))
         
         if self.classify == True or sample_balanced == True:
             
@@ -112,13 +113,23 @@ class CombinedDataset(Dataset):
             self.epa_df.loc[self.epa_df['Daily Mean PM2.5 Concentration'] <= 12.0, 'above_12'] = 0 
         
         if sample_balanced == True:
-            above_12_df = self.epa_df[self.epa_df['above_12'] == 1].iloc[0:50000]
-            below_12_df = self.epa_df[self.epa_df['above_12'] == 0].iloc[0:70000]
+            above_12_df = self.epa_df[self.epa_df['above_12'] == 1]
+            below_12_df = self.epa_df[self.epa_df['above_12'] == 0]
+            print("{} initially has {} examples below 12 and {} examples above 12.".format(master_csv_file, 
+                                                                                 len(below_12_df), 
+                                                                                 len(above_12_df)))
+            
+            self.epa_df = self.epa_df.sample(frac=1)
+            above_12_df = self.epa_df[self.epa_df['above_12'] == 1].iloc[0:7500]
+            below_12_df = self.epa_df[self.epa_df['above_12'] == 0].iloc[0:7500]
             self.epa_df = pd.concat([above_12_df, below_12_df], ignore_index=True)
             self.epa_df = self.epa_df.sample(frac=1)
             
+            print("After sampling, {} has {} examples below 12 and {} examples above 12.".format(master_csv_file, 
+                                                                                 len(below_12_df), 
+                                                                                 len(above_12_df)))
+         
         #self.epa_df = utils.clean_df(self.epa_df) no more cleaning needed, just change index
-        #self.epa_df = self.epa_df.rename(columns={'Unnamed: 0': 'Index'}) 
         self.epa_df = self.epa_df.drop(['Unnamed: 0'], axis=1)
         
     def __len__(self):
@@ -138,7 +149,7 @@ class CombinedDataset(Dataset):
         sample["site"] = epa_row['Site ID']
         #
 
-        sample["non_image"], _ = utils.get_epa_features_no_snow(epa_row)
+        sample["non_image"], _ = utils.get_epa_features(epa_row) #no_snow
         if self.image_dir:
             tif_filename = str(epa_row["SENTINEL_FILENAME"])
             tif_index =  int(epa_row["SENTINEL_INDEX"])
@@ -147,7 +158,7 @@ class CombinedDataset(Dataset):
         
             try:
                 image = np.load(npy_fullpath)
-                image = self.normalize(image)
+                #image = self.normalize(image)
 
             except (FileNotFoundError, ValueError) as exc:
                 image = np.zeros((200,200, 13))
@@ -164,8 +175,18 @@ class CombinedDataset(Dataset):
             label = epa_row["Daily Mean PM2.5 Concentration"]
 
         sample["label"] = label
+        
         sample = self.transform(sample)
-     
+        
+        ## Select the 8 chosen bands
+        ## Should be moved into ToTensor, here for now because post-normalization
+        image = sample['image']
+        first_4_bands = image[:4] 
+        last_4_bands = image[9:]
+        image = np.concatenate((first_4_bands, last_4_bands), axis=0)
+        sample['image'] = image
+        
+        
         return sample
         
     def normalize(self, array):
@@ -190,26 +211,25 @@ class Normalize(object):
     """Normalize image Tensors."""
 
     def __call__(self, sample):
-        image, lat, lon = sample['image'], sample['lat'], sample['lon']
-        prec, snow, snwd = sample['prec'], sample['snow'], sample['snwd']
-        tmin, tmax, month = sample['tmin'], sample['tmax'], sample['month']
-        mb1, mb2, mb3, mb4 = sample['mb1'], sample['mb2'], sample['mb3'], sample['mb4']
-        mg1, mg2, mg3, mg4 = sample['mg1'], sample['mg2'], sample['mg3'], sample['mg4']
-        pm = sample['pm']
+        index, image, non_image, label = sample['index'], sample['image'], sample['non_image'], sample['label'] 
+        month, site = sample['month'], sample['site']
         
-        img_normalization = transforms.Normalize(mean=[0.485, 0.456, 0.406, 0.5, 0.5, 0.5, 
-                                                       0.5,0.5,0.5,0.5,0.5,0.5,0.5],
-                                              std=[0.229, 0.224, 0.225, 0.226,0.225,0.225, 
-                                                   0.225,0.225,0.225,0.225,0.225,0.225,0.225])
-                                                   
-        image = img_normalization(image) 
-        # normalize features 
-        #pm = normalize_pm(pm, min_pm = MIN_PM_VALUE, max_pm = MAX_PM_VALUE)
-        return {'image': image, 'lat': lat, 'lon':lon, 'prec': prec, 'snow':snow,
-                'snwd': snwd, 'tmin':tmin, 'tmax':tmax, 'month': month, 
-                'mb1': mb1, 'mb2': mb2, 'mb3': mb3, 'mb4': mb4, 
-                'mg1': mg1, 'mg2': mg2, 'mg3': mg3, 'mg4': mg4, 
-                'pm': pm} 
+        img_norm = transforms.Normalize(mean=[3144.0764, 2940.7810, 2733.0339, 2820.7695, 2963.3057, 3402.0249,
+                                        3641.9360, 3506.4553, 3780.6147, 1732.4203,  313.6926, 2383.2466, 1815.5107], 
+                                        std=[2496.1377, 2527.8665, 2389.6580, 2587.3665, 2536.4597, 2420.5823,
+                                        2437.2566, 2353.8274, 2421.9773, 1651.2279,  693.4088, 1381.2958, 1147.4915])
+        
+        ft_means = np.array([38.7123, -96.0561, 6.8902, -0.4955, -0.4929, -0.4907, -0.4956,
+                                   -0.4731,-0.4710,-0.4700,-0.4747,26.2292,0.7070,6.7583,207.3810,86.5185])
+        
+        ft_stdvs = np.array([5.4067,17.2314,3.3965,0.5544,0.5546,0.5550,0.5543,0.5781,0.5783,
+                                                  0.5787,0.5778,83.8406,11.0480,70.6240,104.9007,95.1414])
+        
+        non_image = (non_image-torch.from_numpy(ft_means).to(dtype=torch.float))/torch.from_numpy(ft_stdvs).to(dtype=torch.float)
+                                                         
+        image = img_norm(image) 
+        
+        return {'index':index, 'image': image, 'non_image':non_image, 'month':month, 'site':site, 'label':label}
 
 
     
@@ -394,11 +414,12 @@ def load_data_new(train_nonimage_csv, batch_size = BATCH_SIZE, num_workers = 0,
         * "test" (optional) : DataLoader for testing data
     """
     print("Using {} workers to load data...".format(num_workers))
-    train_dataset = CombinedDataset(train_nonimage_csv, kwargs.get("train_images"), sample_balanced=sample_balanced,
+    train_dataset = CombinedDataset(train_nonimage_csv, kwargs.get("train_images"), 
+                                    threshold=20.5, sample_balanced=sample_balanced,
                                    predict_monthly=predict_monthly)
     train_end = len(train_dataset)
     if kwargs.get("test_nonimage_csv"):
-        test_dataset = CombinedDataset(kwargs["test_nonimage_csv"], kwargs.get("test_images"))
+        test_dataset = CombinedDataset(kwargs["test_nonimage_csv"], kwargs.get("test_images"), threshold=20.5)
         print("{} entries in test set".format(len(test_dataset)))
         test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
                                     num_workers = num_workers)
@@ -412,7 +433,8 @@ def load_data_new(train_nonimage_csv, batch_size = BATCH_SIZE, num_workers = 0,
         test_dataloader = None
         
     if kwargs.get("val_nonimage_csv"):
-        val_dataset = CombinedDataset(kwargs["val_nonimage_csv"], kwargs.get("val_images"), predict_monthly=predict_monthly)
+        val_dataset = CombinedDataset(kwargs["val_nonimage_csv"], kwargs.get("val_images"), threshold=20.5, 
+                                       sample_balanced=sample_balanced, predict_monthly=predict_monthly)
         print("{} entries in validation set".format(len(val_dataset)))
         val_dataloader = DataLoader(val_dataset, batch_size=batch_size, 
                                     num_workers = num_workers, shuffle=True)

utils.py
@@ -12,6 +12,8 @@ import seaborn as sns
 import torch.nn as nn
 from sklearn.metrics import r2_score
 from pandarallel import pandarallel 
+from scipy.stats.stats import pearsonr
+
 
 def load_csv_dfs(folder_path, blacklist = []):
     """
@@ -435,6 +437,9 @@ def load_checkpoint(checkpoint, model, optimizer=None):
 
 
 def plot_losses(train_losses, val_losses, num_epochs, num_ex, save_as):
+    '''
+    Method to plot train and validation losses over num_epochs epochs.
+    '''
     plt.clf()
     plt.plot(range(0, num_epochs), train_losses, label='train')
     plt.plot(range(0, num_epochs), val_losses, label='val')
@@ -447,6 +452,9 @@ def plot_losses(train_losses, val_losses, num_epochs, num_ex, save_as):
     plt.savefig(save_as)
                                             
 def plot_r2(train_r2, val_r2, num_epochs, num_ex, save_as):
+    '''
+    Method to plot train and validation r2 values over num_epochs epochs.
+    '''
     plt.clf()
     plt.plot(range(0, num_epochs), train_r2, label = 'train')
     plt.plot(range(0, num_epochs), val_r2, label = 'val')
@@ -458,97 +466,6 @@ def plot_r2(train_r2, val_r2, num_epochs, num_ex, save_as):
     plt.show()
     plt.savefig(save_as)
     
-
-def plot_filters_single_channel(t):
-    
-    #kernels depth * number of kernels
-    nplots = t.shape[0]*t.shape[1]
-    ncols = 12
-    
-    nrows = 1 + nplots//ncols
-    #convert tensor to numpy image
-    t = t.data.cpu().numpy()
-    npimg = np.array(t, np.float32)
-    
-    count = 0
-    fig = plt.figure(figsize=(ncols, nrows))
-    
-    #looping through all the kernels in each channel
-    for i in range(t.shape[0]):
-        for j in range(t.shape[1]):
-            count += 1
-            ax1 = fig.add_subplot(nrows, ncols, count)
-            npimg = np.array(t[i, j], np.float32)
-            npimg = (npimg - np.mean(npimg)) / np.std(npimg)
-            npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
-            ax1.imshow(npimg)
-            ax1.set_title(str(i) + ',' + str(j))
-            ax1.axis('off')
-            ax1.set_xticklabels([])
-            ax1.set_yticklabels([])
-   
-    plt.tight_layout()
-    plt.show()
-    plt.savefig("image.png")
-    
-    
-    
-def plot_filters_multi_channel(t):
-    
-    #get the number of kernals
-    num_kernels = t.shape[0]    
-    
-    #define number of columns for subplots
-    num_cols = 12
-    #rows = num of kernels
-    num_rows = num_kernels
-    
-    #set the figure size
-    fig = plt.figure(figsize=(num_cols,num_rows))
-    
-    #looping through all the kernels
-    for i in range(t.shape[0]):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        
-        #for each kernel, we convert the tensor to numpy 
-        npimg = np.array(t[i].numpy(), np.float32)
-        #standardize the numpy image
-        npimg = (npimg - np.mean(npimg)) / np.std(npimg)
-        npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
-        npimg = npimg.transpose((1, 2, 0))
-        ax1.imshow(npimg)
-        ax1.axis('off')
-        ax1.set_title(str(i))
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
-        
-    plt.savefig('myimage.png', dpi=100)    
-    plt.tight_layout()
-    plt.show()
-    plt.savefig("image2.png")
-    
-    
-def plot_weights(model, layer_num, single_channel = True, collated = False):
-    #extracting the model features at the particular layer number
-    layer = model.conv1
-    
-    #checking whether the layer is convolution layer or not 
-    if isinstance(layer, nn.Conv2d):
-        #getting the weight tensor data
-        weight_tensor = layer.weight.data
-        if single_channel:
-            if collated:
-                plot_filters_single_channel_big(weight_tensor)
-            else:
-                plot_filters_single_channel(weight_tensor)
-        else:
-            if weight_tensor.shape[1] == 3:
-                plot_filters_multi_channel(weight_tensor)
-            else:
-                print("Can only plot weights with three channels with single channel = False")
-    else:
-        print("Can only visualize layers which are convolutional")
-
         
 def save_predictions(indices, predictions, labels, sites, dates, batch_size, save_to):
     '''
@@ -558,7 +475,6 @@ def save_predictions(indices, predictions, labels, sites, dates, batch_size, sav
     '''
     with open(save_to, 'a') as fd:
         writer = csv.writer(fd)
-        #writer.writerow(["Index", "Prediction", "Label"])
         for i in range(0, batch_size):
             index = indices[i]
             y_pred = predictions[i]
@@ -592,7 +508,22 @@ def compute_r2(predictions_csv):
     labels = df['Label']
     
     r2 = r2_score(labels, predictions)
-    return r2
+    pearson = pearsonr(labels, predictions)
+    return r2, pearson 
+    
+def compute_r2_monthly(predictions_csv):
+    '''                                                 
+    Takes in .csv created from save_predictions of (indices, predictions, labels)
+    for each example, and calculates total R2 over the dataset.
+    '''
+    df = fix_month_avg_preds_file(predictions_csv)
+    
+    predictions = df['Month Average']
+    labels = df['Predicted Month Average']
+    
+    r2 = r2_score(labels, predictions)
+    pearson = pearsonr(labels, predictions)
+    return r2, pearson     
     
 def plot_loss_histogram(predictions_csv):
     '''                                                 
@@ -630,7 +561,7 @@ def plot_loss_histogram(predictions_csv):
     plt.axvline(stdev3, color='k', linestyle='dashed', linewidth=1)
     plt.text(stdev3*1.1, max_ylim*0.5, 'Mean+3std = {:.2f}'.format(stdev3), fontsize=10)
 
-    plt.savefig("loss-hist.png")
+    plt.savefig("plots/loss_hist.png")
     plt.show() 
     
     above_three_stdv = df[df['MSE']>stdev3]
@@ -648,8 +579,7 @@ def resave_preds_with_month_and_site(predictions_csv, master_csv, averages_csv="
     '''                                                                                                        
     Helper function to take  given predictions file/df and gather Site ID                       
     and month information for all datapoints in the file. Saves new .csv with these                            
-    columns added and returns the df. To be used in computing monthly averages                                \
-                                                                                                               
+    columns added and returns the df. To be used in computing monthly averages                              
     from daily PM predictions.                                                                                 
     '''
     new_pred_csv = "new_preds.csv"
@@ -752,6 +682,17 @@ def compute_pm_month_average_pre(master_csv, averages_csv="averages.csv"):
                 row = [station_id, month, month_average]
                 writer.writerow(row)
 
+                
+def fix_month_avg_preds_file(preds_true_file):
+    '''
+    Removes all null entries in the merged monthly averages file.
+    '''
+    df = pd.read_csv(preds_true_file)
+    df = df[df['Month Average'].notnull()]
+    df = df[df['Predicted Month Average'].notnull()]
+    return df
+    
+    
 def get_month_average(epa_row):
     averages_csv = "averages.csv"
     average_df = pd.read_csv(averages_csv)
@@ -796,3 +737,104 @@ def average_analysis(averages_csv):
         pms = month_df['Month Average']
         mean_avg_pm_allsites = np.mean(pms)
         print("Month {} mean average over all sites: {}".format(month, mean_avg_pm_allsites))
+
+        
+def plot_predictions(predictions_csv):
+    '''
+    Method to plot true PM2.5 values vs. model predicted values based on predictions in
+    the predictions_csv file.
+    '''
+    df = pd.read_csv(predictions_csv)
+    
+    indices = df['Index']
+    predictions = df['Prediction']
+    labels = df['Label']
+   
+    plt.scatter(labels, predictions, s=1)
+    plt.xlabel("Real PM2.5 Values (μg/$m^3$')")
+    plt.ylabel("Model PM2.5 Predictions (μg/$m^3$')")
+    plt.axis([-10, 25, -10, 25])
+    plt.title("True PM2.5 Values versus Model PM2.5 Predictions")
+    plt.savefig("plots/true_vs_preds_new.png")
+    plt.show()
+
+    
+def plot_predictions_histogram(predictions_csv, dataset='val'):
+    '''                                                 
+    Takes in .csv created from save_predictions of (indices, predictions, labels)
+    for each example. Then plots the histogram of the predictions vs. the labels.  
+    '''
+    plt.clf()
+    df = pd.read_csv(predictions_csv)
+    predictions = df['Prediction']
+    labels = df['Label']
+    bins = 75
+   
+    #plt.axis([0, 200, -10, 300]) # really should be number of examples                                                                                                                                        
+    plt.hist(predictions, bins, alpha=.9,label = dataset+'Predictions')
+    plt.hist(labels, bins, alpha=.9,label = dataset+'Labels')
+    plt.legend()
+    plt.savefig("plots/"+dataset+"_predictions_hist.png")
+    plt.show()
+
+    
+    
+def highest_loss_analysis(predictions_csv):
+    '''                                                 
+    Takes in .csv created from save_predictions of (indices, predictions, labels, site id, month),
+    for each example, and calculates MSE of each example.     
+    Then determines examples with highest losses, and looks for trends. 
+    '''
+    pandarallel.initialize()
+
+    df = pd.read_csv(predictions_csv)
+    mses = df.parallel_apply(mse_row, axis=1)
+    df['MSE'] = mses
+
+    # Compute mean and stdev of MSEs
+    mean_mse = np.mean(mses)
+    stddev_mse = np.std(mses)
+
+    stdev1 = mean_mse + stddev_mse
+    stdev2 = stdev1 + stddev_mse
+    stdev3 = stdev2 + stddev_mse
+    
+    # Determine outliers based on loss - defined as examples > 3 std. away from mean
+    above_two_stdv = df[df['MSE']>stdev2]
+    above_three_stdv = df[df['MSE']>stdev3]
+    sorted_above_three = above_three_stdv.sort_values(by=['Index'])
+    
+    category = 'Site ID' # or Month
+    category_outliers = above_three_stdv[category]
+    unique_values = pd.unique(category_outliers)   #i.e. unique sites / unique months
+    num_unique = len(unique_values)
+
+    plt.hist(sites, num_unique, alpha=.9,label = 'Highest Loss examples by '+ category)
+    plt.title('Highest Loss examples by '+ category ,fontname="Georgia", fontsize=16)
+    plt.xlabel(category, fontname="Times New Roman", fontsize=12)
+    plt.ylabel('Frequency',fontname="Times New Roman", fontsize=12)
+    plt.savefig("plots/highest_losses_by_" + category + ".png")
+    plt.show()
+    
+    n = 5 # Top 5 most fq sites most informative; 4 most fq months most informative
+    most_fq_in_category = category_outliers.value_counts()[:n].index.tolist()  # Top 5 most fq sites most informative
+    print(most_fq_in_category)
+    print(category_outliers.mode()) # or for just one top-most frequent     
+
+    
+def get_outlier_info(master_csv):
+    '''
+    Look up information of an outlier found from highest_loss_analysis function
+    '''
+    
+    site_id = 60371201 # CA
+    df = pd.read_csv(master_csv)
+
+    site_points = df[df['Site ID'] == site_id]
+    twenty = site_points.head(20)                                                             
+    most_fq_months = site_points['Month'].value_counts()[:12].index.tolist()
+
+    preds_csv = "/Users/sarahciresi/Downloads/newest_combined_val_epoch_14.csv"
+    preds_df = pd.read_csv(preds_csv)
+    preds_at_site = preds_df[preds_df['Site ID'] == site_id]
+    print(preds_at_site)